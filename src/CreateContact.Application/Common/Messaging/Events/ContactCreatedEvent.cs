5a1f7eba4d30dd7064fa61b59958a4cfbba62faf
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-19 12:25:23 -0300

    feat: application layer

---
+++ src/CreateContact.Application/Common/Messaging/Events/ContactCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace CreateContact.Application.Common.Messaging.Events;
+
+public record ContactCreatedEvent(
+    string Name,
+    int DDDCode,
+    string Phone,
+    string? Email
+    ) : IntegrationEvent;
---
+++ src/CreateContact.Application/Common/Messaging/RabbitMQEventBus.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace CreateContact.Application.Common.Messaging;
+
+public class RabbitMQEventBus(
+    string hostname,
+    string connectionName,
+    ILogger<RabbitMQEventBus> logger) : IEventBus
+{
+    public async Task PublishAsync<T>(T message, string queueName)
+    {
+        var _factory = new ConnectionFactory();
+        _factory.HostName = hostname;
+        _factory.ClientProvidedName = connectionName;
+
+        IConnection conn = await _factory.CreateConnectionAsync();
+
+        IChannel channel = await conn.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queueName, true, false, false, null);
+
+        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+        var props = new BasicProperties();
+
+        props.Persistent = true;
+
+        await channel.BasicPublishAsync(string.Empty, queueName, true, props, body);
+
+        logger.LogInformation("Message published to queue {QueueName} with message: {Message}", queueName, message);
+
+        await Task.CompletedTask;
+    }
+}
